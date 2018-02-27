75b218968462146eb02364e56a4e6cf86f35ca39
Manu <ManuS@email.com>
2018-02-27 10:04:08 +0100

    You can choose a function to implement.

--- Board.h
+++ Board.h
@@ -1,6 +1,11 @@
 #include <iostream>
-#include <vector>
+#include <array>
 
 class Board {
-    
+    Piece* board[8][8];
+public:
+    void init();
+    bool valid( int x, int y, Piece piece);
+    void move(int x, int y, Piece piece);
+    void print();
 };
