b37172a0cd07e61bb75e190cc942aa656a14ff38
Manu <ManuS@email.com>
2018-03-02 08:27:26 +0100

    Update class Bishop with validMove function

--- Bishop.cpp
+++ Bishop.cpp
@@ -11,3 +11,12 @@ Bishop::Bishop(bool b) : Piece(b) {
 	figure = 'B';
 }
 
+bool Bishop::validMove(unsigned int xFrom, unsigned int yFrom, unsigned int xTo, unsigned int yTo) {
+	bool isValid;
+	int cellX = xTo - xFrom;
+	int cellY = yTo - yFrom;
+	if (abs(cellX) == abs(cellY)) {
+		isValid = true;
+	}
+	return isValid;
+}
--- Bishop.h
+++ Bishop.h
@@ -12,6 +12,8 @@
 class Bishop : public Piece {
 public:
 	Bishop(bool b);
+	~Bishop();
+	bool validMove(unsigned int xFrom, unsigned int yFrom, unsigned int xTo, unsigned int yTo);
 };
 
 #endif /* SRC_CLASSGAME_BISHOP_H_ */
