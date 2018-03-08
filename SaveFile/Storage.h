13dbae28936f3e857293cde8b86d891b7d48115f
Manu <ManuS@email.com>
2018-03-08 09:37:56 +0100

    Save Game update.

---
+++ SaveFile/Storage.h
@@ -0,0 +1,21 @@
+/*
+ * Storage.h
+ *
+ *  Created on: 07/03/2018
+ *      Author: usuario
+ */
+
+#ifndef SRC_SAVEFILE_STORAGE_H_
+#define SRC_SAVEFILE_STORAGE_H_
+#include <iostream>
+#include <fstream>
+#include "../ClassGame/Board.h"
+using namespace std;
+
+class Storage {
+public:
+	static void saveGame();
+
+};
+
+#endif /* SRC_SAVEFILE_STORAGE_H_ */
